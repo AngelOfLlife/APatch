e7c8f5eefff23d4cf93093a050a85a0fcc72e999
AngelOfLlife <116603991+AngelOfLlife@users.noreply.github.com>
2025-07-30 23:05:59 +0500

    Update APatchApp.kt

--- app/src/main/java/me/bmax/apatch/APatchApp.kt
+++ app/src/main/java/me/bmax/apatch/APatchApp.kt
@@ -256,7 +256,7 @@ class APApplication : Application(), Thread.UncaughtExceptionHandler {
             exitProcess(0)
         }
 
-        if (!BuildConfig.DEBUG && !verifyAppSignature("1x2twMoHvfWUODv7KkRRNKBzOfEqJwRKGzJpgaz18xk=")) {
+        if (!BuildConfig.DEBUG && !verifyAppSignature("rHDyK+tan8zCy4AFxVTEUq8njHVAT3bO+drVJG8mOes=")) {
             while (true) {
                 val intent = Intent(Intent.ACTION_DELETE)
                 intent.data = "package:$packageName".toUri()
